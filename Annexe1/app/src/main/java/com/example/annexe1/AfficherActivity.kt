f6c45602543810dc6455327fe1381919971c7ce1
Yuta.L <yutaiida28@gmail.com>
2025-08-27 11:36:04 -0400

    part 2

--- Annexe1/app/src/main/java/com/example/annexe1/AfficherActivity.kt
+++ Annexe1/app/src/main/java/com/example/annexe1/AfficherActivity.kt
@@ -1,20 +1,46 @@
 package com.example.annexe1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-class ListeActivity : AppCompatActivity() {
+class AfficherActivity : AppCompatActivity() {
+    lateinit var liste: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_liste)
+        setContentView(R.layout.activity_afficher)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        liste = findViewById(R.id.liste)
+//        liste.setAdapter(ArrayAdapter(this, android.R.layout.simple_list_item_1, lireMemos()))
+
+        liste.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lireMemos())
+
+    }
+
+    private fun lireMemos(): ArrayList<String> {
+        val fis = openFileInput("memo.txt")
+        val isr = InputStreamReader(fis)
+        val br = BufferedReader(isr)
+
+        val arrayliste = ArrayList<String>()
+        var line = br.readLine()
+
+        while (line != null) {
+            arrayliste.add(line)  // Add the actual line content
+            line = br.readLine()
+        }
+        br.close()
+        return arrayliste
     }
 }
--- Annexe1/app/src/main/java/com/example/annexe1/MainActivity.kt
+++ Annexe1/app/src/main/java/com/example/annexe1/MainActivity.kt
@@ -6,7 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+import android.widget.Button
+import android.view.View
+import android.view.View.OnClickListener
+import android.content.Intent
+
 class MainActivity : AppCompatActivity() {
+    lateinit var boutonAjouter:Button
+    lateinit var boutonAficher:Button
+    lateinit var boutonQuitter:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,34 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        boutonAjouter = findViewById(R.id.boutonAjouter)
+        boutonAficher = findViewById(R.id.boutonAficher)
+        boutonQuitter = findViewById(R.id.boutonQuitter)
+
+
+        //1ere
+        val ec = Ecouteur()
+        // 2e etape
+        boutonAjouter.setOnClickListener(ec)
+        boutonAficher.setOnClickListener(ec)
+        boutonQuitter.setOnClickListener(ec)
+    }
+
+    // 3e etape
+    inner class Ecouteur : OnClickListener{
+        override  fun onClick(v: View?) {
+            when ( v ){
+                boutonQuitter -> finish()
+                boutonAjouter -> {
+                    val i = Intent(this@MainActivity, AjouterActivity::class.java)
+                    startActivity(i)
+                }
+                boutonAficher -> {
+                    val i = Intent(this@MainActivity, AfficherActivity::class.java)
+                    startActivity(i)
+                }
+            }
         }
     }
 }
