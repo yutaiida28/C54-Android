5d3e0862e59c27d83d27ea67d93a50912c10f3b5
yutaiida28 <yutaiida28@gmail.com>
2025-09-24 11:38:51 -0400

    annexe 5

---
+++ annex5/appListeComplexe/app/src/androidTest/java/com/example/applistecomplexe/music.kt
@@ -0,0 +1,5 @@
+package com.example.applistecomplexe
+
+import java.io.Serializable
+
+data class music(val title: String): Serializable
---
+++ annex5/appListeComplexe/app/src/main/java/com/example/applistecomplexe/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.applistecomplexe
+
+import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listMusic: ListView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        listMusic = findViewById(R.id.appListe)
+        val adap = SimpleAdapter(this,remplirArrayListe(), R.layout.music_item,arrayOf("position","nom","date","image"),intArrayOf(R.id.textPos,R.id.textNom,R.id.textDate,R.id.imageView));
+        listMusic.setAdapter(adap);
+    //        val list=ArrayList<HashMap<String,Any>>()
+    }
+    fun remplirArrayListe () : ArrayList<HashMap<String,Any>>{
+        var listeDonnes =ArrayList<HashMap<String,Any>>()
+        var chanson = HashMap<String,Any>()
+
+        chanson.put("position", 3) // chanson["position"]= 3 meme que enhaut
+        chanson["nom"]= "Touch Me"
+        chanson["date"]= "22/03/86"
+        chanson.put ("image", R.drawable.touchme)
+
+        listeDonnes.add(chanson)
+
+        chanson = HashMap() //fair un nouveaux hachmap car il vas l'ecraser sinon
+        chanson.put("position", 8)
+        chanson["nom"]= "Nothings gonna stop me now"
+        chanson["date"]= "22/03/86"
+        chanson.put ("image", R.drawable.nothing)
+        listeDonnes.add(chanson)
+
+        chanson = HashMap() //fair un nouveaux hachmap car il vas l'ecraser sinon
+        chanson.put("position", 31)
+        chanson["nom"]= "santa maria"
+        chanson["date"]= "22/03/1992"
+        chanson.put ("image", R.drawable.santamaria)
+
+        chanson = HashMap() //fair un nouveaux hachmap car il vas l'ecraser sinon
+        chanson.put("position", 108)
+        chanson["nom"]= "Hot body"
+        chanson["date"]= "22/03/86"
+        chanson.put ("image", R.drawable.hotboy)
+        listeDonnes.add(chanson)
+
+        return listeDonnes
+    }
+}
