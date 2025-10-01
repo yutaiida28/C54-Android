e34384b27327acda4d19770096aa0977956dd517
yutaiida28 <yutaiida28@gmail.com>
2025-10-01 10:45:07 -0400

    fin annexe 5

--- annex5/appListeComplexe/app/src/main/java/com/example/applistecomplexe/MainActivity.kt
+++ annex5/appListeComplexe/app/src/main/java/com/example/applistecomplexe/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.applistecomplexe
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +31,23 @@ class MainActivity : AppCompatActivity() {
         val adap = SimpleAdapter(this,remplirArrayListe(), R.layout.music_item,arrayOf("position","nom","date","image"),intArrayOf(R.id.textPos,R.id.textNom,R.id.textDate,R.id.imageView));
         listMusic.setAdapter(adap);
     //        val list=ArrayList<HashMap<String,Any>>()
+
+        val ec = Ecouteur()
+//        listMusic.setOnItemClickListener(ec) // methode java
+        listMusic.onItemClickListener = ec // methode kotlin
+    }
+
+    inner class Ecouteur : OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//            var choisie = listMusic.getItemAtPosition(position) as HashMap<String, Any>
+//            var nomMusic =  choisie["nom"]  as String
+//            Toast.makeText(this@MainActivity,nomMusic,Toast.LENGTH_LONG).show()
+
+            val linearLayout = view as LinearLayout
+            val textView = linearLayout.findViewById<TextView>(R.id.textNom)
+            Toast.makeText(this@MainActivity,textView.text.toString(),Toast.LENGTH_LONG).show()
+        }
+
     }
     fun remplirArrayListe () : ArrayList<HashMap<String,Any>>{
         var listeDonnes =ArrayList<HashMap<String,Any>>()
@@ -49,6 +72,7 @@ class MainActivity : AppCompatActivity() {
         chanson["nom"]= "santa maria"
         chanson["date"]= "22/03/1992"
         chanson.put ("image", R.drawable.santamaria)
+        listeDonnes.add(chanson)
 
         chanson = HashMap() //fair un nouveaux hachmap car il vas l'ecraser sinon
         chanson.put("position", 108)
